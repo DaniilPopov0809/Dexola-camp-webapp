1ab4df314b7e626176a135008844dc81e961b263
Daniil Popov <x6uhrox@gmail.com>
2023-09-14 22:01:52 +0300

    test deploy

--- src/helpers/calculateRewardRate.ts
+++ src/helpers/calculateRewardRate.ts
@@ -1,4 +1,22 @@
 import timeStamp from "./timeStamp";
+// import { formatEther } from "viem/utils";
+
+// const calculateRewardRate = (
+//   stakedBalance: bigint,
+//   periodFinish: bigint,
+//   rewardRate: bigint,
+//   totalSupply: bigint
+// ) => {
+//   const currentTimeStamp = BigInt(timeStamp());
+//   const totalAvailbleRewards = (periodFinish - currentTimeStamp) * rewardRate;
+//   const result = (stakedBalance * totalAvailbleRewards) / totalSupply + stakedBalance;
+//   const formatted = formatEther(result);
+//   return Math.floor(Number(formatted));
+// };
+
+// export default calculateRewardRate;
+
+
 import { formatEther } from "viem/utils";
 
 const calculateRewardRate = (
@@ -8,9 +26,11 @@ const calculateRewardRate = (
   totalSupply: bigint
 ) => {
   const currentTimeStamp = BigInt(timeStamp());
-  const totalAvailbleRewards = (periodFinish - currentTimeStamp) * rewardRate;
-  const result = (stakedBalance * totalAvailbleRewards) / totalSupply + stakedBalance;
-  const formatted = formatEther(result);
+  const totalAvailableRewards = (periodFinish - currentTimeStamp) * rewardRate;
+  const stakedBalanceBigInt = BigInt(stakedBalance); // Преобразуем stakedBalance к bigint
+  const totalSupplyBigInt = BigInt(totalSupply); // Преобразуем totalSupply к bigint
+  const result = (stakedBalanceBigInt * totalAvailableRewards) / totalSupplyBigInt + stakedBalanceBigInt;
+  const formatted = formatEther(result); // Преобразуем результат к строке для форматирования
   return Math.floor(Number(formatted));
 };
 
