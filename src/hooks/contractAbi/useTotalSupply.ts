79402fb1cb061dc00950eef812d6a4af4501b53c
Daniil Popov <x6uhrox@gmail.com>
2023-09-15 00:09:03 +0300

    test deploy

--- src/hooks/contractAbi/useTotalSupply.ts
+++ src/hooks/contractAbi/useTotalSupply.ts
@@ -1,16 +1,18 @@
 import { useContractRead } from "wagmi";
 import contractAbi from "../../data/contractABI.json";
-const {VITE_CONTRACT_ADDRESS} = import.meta.env;
+const { VITE_CONTRACT_ADDRESS } = import.meta.env;
 
-  const useTotalSupply = () => {
-      // const { isConnected } = useAccount();
-    const { data: totalSupply }  = useContractRead({
-      address: VITE_CONTRACT_ADDRESS,
-      abi: contractAbi,
-      functionName: "totalSupply",
-      watch: true,
-    });
-    return  BigInt(totalSupply as bigint);
-  };
+const useTotalSupply = () => {
+  // const { isConnected } = useAccount();
+  const { data: totalSupply, isSuccess } = useContractRead({
+    address: VITE_CONTRACT_ADDRESS,
+    abi: contractAbi,
+    functionName: "totalSupply",
+    watch: true,
+  });
+  if (isSuccess) {
+    return totalSupply as bigint;
+  }
+};
 
-  export default useTotalSupply;
+export default useTotalSupply;
