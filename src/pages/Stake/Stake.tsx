24ab37a301b5c9ff52c65572c31b2e2187420017
Daniil Popov <x6uhrox@gmail.com>
2023-09-14 21:36:10 +0300

    test deploy

--- src/pages/Stake/Stake.tsx
+++ src/pages/Stake/Stake.tsx
@@ -7,7 +7,7 @@ import {
 } from "../../hooks/contractAbi";
 import calculateRewardRate from "../../helpers/calculateRewardRate";
 import NoWalletConnect from "../../components/UI/NoWalletConnect/NoWalletConnect";
-import AppForm from "../../components/UI/AppForm/AppForm";
+// import AppForm from "../../components/UI/AppForm/AppForm";
 import styles from "./Stake.module.scss";
 import Title from "../../components/UI/Title/Title";
 import Rate from "../../components/UI/Rate/Rate";
@@ -40,7 +40,7 @@ const Stake = () => {
               <Rate label={"Reward rate:"} rate={`${rate}`} unit={"STRU/WEEK"} />
             }
           />
-          <AppForm />
+          {/* <AppForm /> */}
         </div>
       ) : (
         <NoWalletConnect />
