fdea546261e235ab43fe5caadd97a22d1fd34e67
Daniil Popov <x6uhrox@gmail.com>
2023-09-24 01:29:34 +0300

    FIX: validationForm

--- src/helpers/utils/checkMaxValue.ts
+++ src/helpers/utils/checkMaxValue.ts
@@ -1,10 +1,14 @@
 import { fetchedBalance } from "../operations";
 import { parseEther } from "viem";
+import { checkMinValue } from ".";
 
-const checkMaxValue = async (value:string) :Promise<boolean>=> {
+const checkMaxValue = async (value: string): Promise<boolean> => {
   try {
+    if (!checkMinValue(value)) {
+      return false;
+    }
     const balance = await fetchedBalance();
-    if (balance) {
+    if (balance && +value > 0.000001) {
       const balanceValue = balance.value;
       const enteredValue = parseEther(value);
       return enteredValue <= balanceValue;
@@ -14,7 +18,6 @@ const checkMaxValue = async (value:string) :Promise<boolean>=> {
     console.error(error);
     return false;
   }
-
-}
+};
 
 export default checkMaxValue;
